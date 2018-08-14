dd35e0a268684a46121487d89e0f2e875672effb
leoroll <1455212235@qq.com>
2018-08-14 07:29:09 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://github.com/LeoRoll" target="_blank">
+                <span style="color: #e27575;font-size: 14px;">❤</span>あきら<span style="color: #e27575;font-size: 14px;">❤</span>
+               </a>
+            设计和编码
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">联系我</a></p>
+    </div>
+</footer>
